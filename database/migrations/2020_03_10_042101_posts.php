c42553649e9722edc8adb81b70362a92054132d1
Bobur <Boburbekrustamov1999@gmail.com>
2020-03-10 10:45:02 +0500

    migration commit

---
+++ database/migrations/2020_03_10_042101_posts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('img');
+            $table->string('title')->notNull();
+            $table->string('short')->notNull();
+            $table->text('content')->notNull();
+            $table->integer('views');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
